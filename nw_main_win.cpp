40f09555ab9075685c1909a1bdfe063fa6edbef1
Vladi Belperchinov-Shabanski <cade@bis.bg>
2020-05-11 01:13:38 +0300

    start-player-detached2

--- nw_main_win.cpp
+++ nw_main_win.cpp
@@ -423,7 +423,6 @@ void NWMainWindow::enter( QTreeWidgetItem *item )
     QStringList exec_args = { ndir + "/" + nw_item->fn };
     QProcess mpl;
     mpl.startDetached( "nightwatch-video-player", exec_args );
-    mpl.waitForFinished();
     
     if( auto_play > 0 )
       {
